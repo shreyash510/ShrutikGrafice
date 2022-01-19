1c6f6bf8f465b0e0ed74707c31b9c501847c2ec5
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2022-01-19 10:10:29 +0530

    changes

--- client/src/components/Pages/ViewSection.jsx
+++ client/src/components/Pages/ViewSection.jsx
@@ -8,45 +8,44 @@ const ViewSection = () => {
     const myUrl = useParams()
     console.log(myUrl.id)
 
-    const productData = async () => {
+    // const productData = async () => {
+    useEffect(() => {
         const url = `/admin-data/${myUrl.id}`;
-        await axios.get(url)
+         axios.get(url)
             .then((PData) => {
                 // console.log(PData.data)
                 setPdata(PData.data)
             }).catch((e) => {
                 console.log(e)
             })
-    }
-
-    useEffect(() => {
-        productData()
-    }, [])
+    
+        // productData()
+    }, [myUrl.id])
 
     console.log(pdata.title)
-    return (
-        <>
-            <div style={{ width: '100%' }} className="container my-5">
-                <div className="container row g-3 flex justify-center items-center">
-                    <div className="col-md-5">
-                        <img className='rounded-2xl w-[25em]' src={pdata.img} alt="BannerImage" />
-                    </div>
-                    <div className="col-md-5">
-                        <p className='py-3 text-3xl text-center'>{pdata.title}</p>
-                        <p className='pb-3 font-medium font-bold text-3xl text-center '>Price :{pdata.price}/-</p>
-                        <p className='text-center'>? Numquam, consequatur. Ab et possimus voluptatem incidunt rem nostrum molestiae modi, ad corrupti amet repellat odit ipsam est itaque deserunt distinctio obcaecati architecto in ea velit rerum iusto alias! Incidunt, pariatur!</p>
-                        <div className="btn flex justify-around">
-                            <button className="btn btn-primary py-2 my-3 mx-1">Add To Cart</button>
-                            <button className="btn btn-primary py-2 my-3 mx-1">Order Now</button>
-                        </div>
+return (
+    <>
+        <div style={{ width: '100%' }} className="container my-5">
+            <div className="container row g-3 flex justify-center items-center">
+                <div className="col-md-5 flex justify-center">
+                    <img className='rounded-2xl w-[25em]' src={pdata.img} alt="BannerImage" />
+                </div>
+                <div className="col-md-5">
+                    <p className='py-3 text-3xl text-center'>{pdata.title}</p>
+                    <p className='pb-3 font-medium font-bold text-3xl text-center '>Price :{pdata.price}/-</p>
+                    <p className='text-center'>? Numquam, consequatur. Ab et possimus voluptatem incidunt rem nostrum molestiae modi, ad corrupti amet repellat odit ipsam est itaque deserunt distinctio obcaecati architecto in ea velit rerum iusto alias! Incidunt, pariatur!</p>
+                    <div className="btn flex justify-around">
+                        <button className="btn btn-primary py-2 my-3 mx-1">Add To Cart</button>
+                        <button className="btn btn-primary py-2 my-3 mx-1">Order Now</button>
                     </div>
                 </div>
             </div>
-            <div className="container">
-                <h1 className='font-bold text-3xl'>Related products</h1>
-            </div>
-        </>
-    )
+        </div>
+        <div className="container">
+            <h1 className='font-bold text-3xl'>Related products</h1>
+        </div>
+    </>
+)
 }
 
 export default ViewSection;
